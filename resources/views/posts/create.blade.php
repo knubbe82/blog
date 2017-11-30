257d4029c0aad26ad35374f3153fdaf0103cdad9
knubbe82 <knubbe1982@gmail.com>
2017-11-30 23:29:07 +0100

    first commit

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,29 @@
+@extends ('layouts.master')
+
+@section ('content')
+    <h1>Publish a post</h1>
+    <hr>
+
+    <form method="POST" action="/posts">
+    {{ csrf_field() }}
+
+      <div class="form-group">
+        <label for="title">Title:</label>
+        <input type="text" class="form-control" id="title" name="title">
+      </div>
+
+      <div class="form-group">
+        <label for="body">Body</label>
+        <textarea name="body" id="body" cols="30" rows="10" class="form-control"></textarea>
+      </div>
+      
+      <div class="form-group">
+        <button type="submit" class="btn btn-primary">Publish</button>
+      </div>
+
+      @include ('layouts.errors')
+
+    </form>
+
+    
+@endsection
