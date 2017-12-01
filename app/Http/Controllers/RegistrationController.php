48d5ee9dd6fc2f2f3a03c1b5e895aba26c20b11a
knubbe82 <knubbe1982@gmail.com>
2017-12-01 17:12:57 +0100

    auth

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+
+class RegistrationController extends Controller
+{
+    public function create()
+    {
+        return view('registration.create');
+    }
+
+    public function store()
+    {
+        // validate a form
+        $this->validate(request(), [
+            'name'  => 'required',
+            'email' =>  'required|email',
+            'password'  => 'required|confirmed'
+        ]);
+
+        // create and save a user
+        $user = User::create([
+            'name'  =>  request('name'),
+            'email' =>  request('email'),
+            'password'  => bcrypt(request('password'))
+        ]);
+
+        // sign them in
+        auth()->login($user);
+
+        // redirect to the home page
+        return redirect()->home();
+    }
+}
